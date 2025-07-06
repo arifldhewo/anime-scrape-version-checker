5d3c4344d058f514e209b5217071a6df4afc277f
Arif Laksonodhewo <arifldhewo234@gmail.com>
2025-07-06 15:22:03 +0700

    Add rate limiter

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,48 @@
-import express, { Request, Response } from "express";
+import express, { Request, Response, NextFunction } from "express";
+import { RateLimiterMemory, RateLimiterRes } from "rate-limiter-flexible";
+
+const opts = {
+    points: 10, // 6 points
+    duration: 1, // Per second
+};
+
+const rateLimiter = new RateLimiterMemory(opts);
+
+function rateLimiterMiddleware() {
+    return async (req: Request, res: Response, next: NextFunction) => {
+        const remoteAddress = req.ip;
+        if (remoteAddress) {
+            try {
+                await rateLimiter.consume(remoteAddress, 1);
+                next();
+            } catch (rateLimiterRes: any) {
+                const secs =
+                    Math.round(rateLimiterRes.msBeforeNext / 1000) || 1;
+
+                res.status(429).json({
+                    error: "Too Many Requests",
+                    message: `Rate limit exceeded. Try again in ${secs} seconds.`,
+                    retryAfter: secs,
+                });
+            }
+        } else {
+            res.status(500).json({
+                error: "Internal Server Error",
+                message: "Failed to gather Rate Limit Identifier",
+            });
+        }
+        return;
+    };
+}
 
 const app = express();
 
-const port = process.env.PORT || 3000;
+app.use(rateLimiterMiddleware());
 
-app.get("/", async (_: Request, res: Response) => {
+const port = process.env.PORT || 3000;
+const githubToken = process.env.ANIME_SCRAPE_GITHUB_TOKEN;
+
+app.get("/", async (req: Request, res: Response) => {
     try {
         res.status(200).json({
             message: "OK",
@@ -14,6 +52,7 @@ app.get("/", async (_: Request, res: Response) => {
             message: `Internal Server Error!`,
             error: e,
         });
+        return;
     }
 });
 
@@ -25,7 +64,7 @@ app.get("/version", async (_: Request, res: Response) => {
                 method: "GET",
                 headers: {
                     Accept: "application/vnd.github+json",
-                    Authorization: `Bearer ${process.env.ANIME_SCRAPE_GITHUB_TOKEN}`,
+                    Authorization: `Bearer ${githubToken}`,
                     "X-Github-Api-Version": "2022-11-28",
                     "User-Agent": "anime-scrape",
                 },
@@ -39,16 +78,17 @@ app.get("/version", async (_: Request, res: Response) => {
         } else {
             res.status(200).send(await getRelease.json());
         }
+        return;
     } catch (e) {
         res.status(500).json({
             status: 500,
             message: "Internal Server Error",
             error: e,
         });
+        return;
     }
 });
 
-// Start the Express server
 app.listen(port, () => {
     console.log(`The server is running at http://localhost:${port}`);
 });
