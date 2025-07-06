7ffda9b8661cf3f4656bc9ac66730c4f8676d034
Arif Laksonodhewo <arifldhewo234@gmail.com>
2025-07-06 14:34:15 +0700

    Adding Error handling

--- src/index.ts
+++ src/index.ts
@@ -4,21 +4,48 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
-app.get("/version", async (_: Request, res: Response) => {
-    const getRelease = await fetch(
-        "https://api.github.com/repos/arifldhewo/anime-scrape/releases",
-        {
-            method: "GET",
-            headers: {
-                Accept: "application/vnd.github+json",
-                Authorization: `Bearer ${process.env.ANIME_SCRAPE_GITHUB_TOKEN}`,
-                "X-Github-Api-Version": "2022-11-28",
-                "User-Agent": "anime-scrape",
-            },
-        }
-    );
+app.get("/", async (_: Request, res: Response) => {
+    try {
+        res.status(200).json({
+            message: "OK",
+        });
+    } catch (e) {
+        res.status(500).json({
+            message: `Internal Server Error!`,
+            error: e,
+        });
+    }
+});
 
-    res.send(await getRelease.json());
+app.get("/version", async (_: Request, res: Response) => {
+    try {
+        const getRelease = await fetch(
+            "https://api.github.com/repos/arifldhewo/anime-scrape/releases",
+            {
+                method: "GET",
+                headers: {
+                    Accept: "application/vnd.github+json",
+                    Authorization: `Bearer ${process.env.ANIME_SCRAPE_GITHUB_TOKEN}`,
+                    "X-Github-Api-Version": "2022-11-28",
+                    "User-Agent": "anime-scrape",
+                },
+            }
+        );
+
+        if (getRelease.status > 399) {
+            res.status(getRelease.status).json({
+                error: await getRelease.json(),
+            });
+        } else {
+            res.status(200).send(await getRelease.json());
+        }
+    } catch (e) {
+        res.status(500).json({
+            status: 500,
+            message: "Internal Server Error",
+            error: e,
+        });
+    }
 });
 
 // Start the Express server
